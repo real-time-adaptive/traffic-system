84aef9a25ea14f51bed5d4128865ed0019ce65b1
saif-abdelrazek <git@saifabdelrazek.com>
2025-12-29 08:20:36 +0200

    feat: add environment variable checks for database connection and update wrangler configuration

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,7 +2,15 @@ import { drizzle } from "drizzle-orm/libsql";
 
 // You can specify any property from the libsql connection options
 const dbUrl = process.env.TURSO_DB_URL || "file:dev.db";
+if (!dbUrl) {
+  throw new Error("TURSO_DB_URL is not defined in environment variables");
+}
 const authToken = process.env.TURSO_DB_AUTH_TOKEN || undefined;
+if (!authToken) {
+  throw new Error(
+    "TURSO_DB_AUTH_TOKEN is not defined in environment variables"
+  );
+}
 
 export const db = drizzle({
   connection: {
